dd6156f9ca580fd73772c60bcd6b64da4b256ca8
lucas <lucas64_64@hotmail.com>
2017-04-11 15:00:41 -0300

    criada as incrições

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,61 +1,95 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  layout 'site'
-# before_action :configure_sign_up_params, only: [:create]
-# before_action :configure_account_update_params, only: [:update]
+    layout 'site'
+    # before_action :configure_sign_up_params, only: [:create]
+    # before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+    before_action :configure_sign_up_params, only: [:create]
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+    def create
+        @user = User.new(inscription_params)
+        # @user.active = true
+        set_minimum_password_length
+        if @user.save
+            flash[:success] = 'Inscrição realizada, em instantes receberá as instruções de confirmação no seu e-mail'
+            redirect_to '/'
+        else
+            flash[:error] = 'Um erro ocorreu, não foi possível processar sua inscrição'
+            redirect_to new_user_registration_path
+        end
+    end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+    protected
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
+    # If you have extra params to permit, append them to the sanitizer.
+    def configure_sign_up_params
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+    end
 
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
+    # def configure_account_update_params
+    #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    # end
+    def inscription_params
+        params.require(:user).permit(:email, :password, :name, :general_register, :cpf, :gender, :birthday, :cep, :state, :adress, :adress_number, :complement, :city, :district, :phone, :cel, :university, :registration_id, :semester, :facebook_profile_link)
+    end
 
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
+    def user_params
+        # NOTE: Using `strong_parameters` gem
+        params.require(:user).permit(:email, :password, :name, :general_register, :cpf, :birthday, :gender, :cel, :university, :facebook_profile_link)
+    end
 
-  # protected
+    # GET /resource/sign_up
+    # def new
+    #   super
+    # end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+    # POST /resource
+    # def create
+    #   super
+    # end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+    # GET /resource/edit
+    # def edit
+    #   super
+    # end
 
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+    # PUT /resource
+    # def update
+    #   super
+    # end
 
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+    # DELETE /resource
+    # def destroy
+    #   super
+    # end
+
+    # GET /resource/cancel
+    # Forces the session data which is usually expired after sign
+    # in to be expired now. This is useful if the user wants to
+    # cancel oauth signing in/up in the middle of the process,
+    # removing all OAuth session data.
+    # def cancel
+    #   super
+    # end
+
+    # protected
+
+    # If you have extra params to permit, append them to the sanitizer.
+    # def configure_sign_up_params
+    #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+    # end
+
+    # If you have extra params to permit, append them to the sanitizer.
+    # def configure_account_update_params
+    #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    # end
+
+    # The path used after sign up.
+    # def after_sign_up_path_for(resource)
+    #   super(resource)
+    # end
+
+    # The path used after sign up for inactive accounts.
+    # def after_inactive_sign_up_path_for(resource)
+    #   super(resource)
+    # end
 end
--- config/application.rb
+++ config/application.rb
@@ -7,20 +7,23 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Sonu2017
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.generators do |g|
-      g.test_framework :rspec,
-        :fixtures => true,
-        :view_specs => false,
-        :helper_specs => false,
-        :routing_specs => true,
-        :controller_specs => true,
-        :request_specs => true
-      g.fixture_replacement :factory_girl, :dir => "spec/factories"
-    end
+    class Application < Rails::Application
+        # Settings in config/environments/* take precedence over those specified here.
+        # Application configuration should go into files in config/initializers
+        # -- all .rb files in that directory are automatically loaded.
+        config.generators do |g|
+            g.test_framework :rspec,
+                             fixtures: true,
+                             view_specs: false,
+                             helper_specs: false,
+                             routing_specs: true,
+                             controller_specs: true,
+                             request_specs: true
+            g.fixture_replacement :factory_girl, dir: 'spec/factories'
+        end
 
-  end
+        config.action_view.field_error_proc = proc { |html_tag, _instance|
+            html_tag
+        }
+    end
 end
