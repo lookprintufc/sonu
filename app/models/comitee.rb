dbeed019c6f448038209931e5eba616cff524404
lucas <lucas64_64@hotmail.com>
2017-04-13 07:58:42 -0300

    criado o model do comitee

--- app/models/comitee.rb
+++ app/models/comitee.rb
@@ -1,2 +1,39 @@
 class Comitee < ApplicationRecord
+
+  def is_full?
+    self.users.count >= self.limit
+  end
+
+  def self.get_actives
+    now = Time.now
+    Comitee.all.each do |comitee|
+      if now > comitee.start_date && now < comitee.end_date
+        return comitee
+      end
+    end
+    nil
+  end
+
+  def self.get_passed
+    now = Time.now
+    Comitee.all.each do |comitee|
+      if now > comitee.end_date
+        return comitee
+      end
+    end
+    nil
+  end
+
+    # def qnt_pays_total
+    #   self.users.joins(:payment).where("payments.portion_paid=payments.portions").count
+    # end
+    #
+    # def qnt_pays_partial
+    #   self.users.joins(:payment).where("payments.portion_paid>0").where("payments.portion_paid!=payments.portions").count
+    # end
+
+    # def self.totalLimit
+    #   self.sum(:limit)
+    # end
+
 end
