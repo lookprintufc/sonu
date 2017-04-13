6ed43bf8af437eb7b2e360e2f0d32c5692f7b0d3
lucas <lucas64_64@hotmail.com>
2017-04-12 22:15:47 -0300

    inserido o logout no user

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,5 @@
 //= require jquery
+//= require jquery_ujs
 //= require maskedinput
 //= require toastr
 jQuery(function($){
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def authenticate_user!
-    @header_top = "header_top_unlogged"
     if user_signed_in?
       @header_top = "header_top_logged"
     end
