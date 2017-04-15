3ec721b9c58979762156338b7d1b3d3583d0ecbc
lucas <lucas64_64@hotmail.com>
2017-04-15 20:23:16 -0300

    Tela login admin 100%

--- app/controllers/crew/admins/sessions_controller.rb
+++ app/controllers/crew/admins/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Crew::Admins::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
-
+  layout "admin_dashboard"
   # GET /resource/sign_in
   # def new
   #   super
