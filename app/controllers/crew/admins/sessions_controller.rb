735f060d2971417274f80fd2799265e00dfa1511
lucas <lucas64_64@hotmail.com>
2017-04-15 23:06:18 -0300

    redirecionamento para o admin

--- app/controllers/crew/admins/sessions_controller.rb
+++ app/controllers/crew/admins/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Crew::Admins::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
-  layout "admin_dashboard"
+  layout "admin_login"
   # GET /resource/sign_in
   # def new
   #   super
@@ -20,6 +20,11 @@ class Crew::Admins::SessionsController < Devise::SessionsController
 
   def after_sign_in_path_for(admin)
     crew_authenticated_admin_root_path
+  end
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    new_crew_admin_session_path
   end
 
   # If you have extra params to permit, append them to the sanitizer.
